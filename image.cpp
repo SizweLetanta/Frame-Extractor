65b12fb5cca031ce0d5677f1cd1b702ad17ab133
Sizwe Letanta <sizwe.letanta@gmail.com>
2023-03-11 21:38:16 +0200

    Added comments and formatting

--- image.cpp
+++ image.cpp
@@ -2,39 +2,40 @@
 #define IMG LTNSIZ001::Image
 
 IMG::Image(int width, int height)
-    : width(width), height(height), pixels(nullptr){}
+	: width(width), height(height), pixels(nullptr) {}
 
-u_char* IMG::operator[](int y){
-    return (u_char*) pixels+(y*width);
+u_char* IMG::operator[](int y) {
+	return (u_char*)pixels + (y * width);
 }
 
-int IMG::get_height(){ return height;}
-
-int IMG::get_width(){ return width;}
-
-IMG::~Image(){
-    delete[] pixels;
+int IMG::get_height() {
+	return height;
 }
 
-u_char** IMG::get_frame(int start_x, int start_y, int width, int height){
-    u_char** frame = new u_char*[height];
-
-    for (int y = 0; y < height; y++)
-    {
-        u_char* row = new u_char[width];
-        for (int x = 0; x < width; x++)
-        {
-            row[x] = (*this)[y][x];
-        }
-        
-        frame[y] = row;
-    }
-
-    return frame;
-    
+int IMG::get_width() {
+	return width;
 }
 
-void IMG::load_image(std::ifstream* file){
-    pixels = new char[width*height];
-    file->read(pixels, width*height);
+IMG::~Image() {
+	delete[] pixels;
+}
+
+u_char** IMG::get_frame(int start_x, int start_y, int width, int height) {
+	u_char** frame = new u_char*[height];
+
+	for (int y = 0; y < height; y++) {
+		u_char* row = new u_char[width];
+		for (int x = 0; x < width; x++) {
+			row[x] = (*this)[y][x];
+		}
+
+		frame[y] = row;
+	}
+
+	return frame;
+}
+
+void IMG::load_image(std::ifstream* file) {
+	pixels = new char[width * height];
+	file->read(pixels, width * height);
 }
--- image.h
+++ image.h
@@ -4,39 +4,48 @@
 #include <fstream>
 typedef unsigned char u_char;
 
-namespace LTNSIZ001
-{
-    class Image{
-    private:
-        char* pixels;
-        int width, height;
-    public:
-        /*
-         * Creates an image object without any pixels
-         */
-        Image(int width, int height);
+namespace LTNSIZ001 {
+	class Image {
+	private:
+		char* pixels;
+		int width, height;
 
-        /*
-         * loads pixel data into the image from an ifstream object. Assumes that the 
-         * ifstream only has image pixels and the header has been removed.
-         */
-        void load_image(std::ifstream* file);
-        u_char** get_frame(int start_x, int start_y, int width, int height);
+	public:
+		/*
+		 * Creates an image object without any pixels
+		 */
+		Image(int width, int height);
 
-        /*
-         * Returns a pointer to a pixel at position
-         */
-        u_char* operator[](int row_no);
+		/*
+		 * loads pixel data into the image from an ifstream object. Assumes that the
+		 * ifstream only has image pixels and the header has been removed.
+		 */
+		void load_image(std::ifstream* file);
 
-        int get_width();
-        int get_height();
+		/*
+		 * Returns a width * height unsigned char array of an image starting from
+		 * (start_x, start_y)
+		 */
+		u_char** get_frame(int start_x, int start_y, int width, int height);
 
-        ~Image();
-    };
-    
-    
-} // namespace LTNSIZ001
+		/*
+		 * Returns a pointer to a pixel at the first column of the given row.
+		 */
+		u_char* operator[](int row_no);
 
+		/*
+		 * Returns the width of the image
+		 */
+		int get_width();
 
+		/*
+		 * Returns the width of the image
+		 */
+		int get_height();
+
+		~Image();
+	};
+
+}  // namespace LTNSIZ001
 
 #endif
